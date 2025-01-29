41f21d1e14b728b24a4385da2713c481a3b10cfa
Natarajan <nataraj.itsolution@gmail.com>
2025-01-29 19:46:41 +0530

    Linked Signal

--- src/app/component/link/link.component.ts
+++ src/app/component/link/link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, linkedSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-link',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './link.component.css'
 })
 export class LinkComponent {
+
+  quantity = signal(1);
+  price = signal(10);
+
+  user = signal({ firstname: 'nihira', lastname: 'tech' })
+  username = linkedSignal({
+    source: this.user,
+    computation: (user) => user,
+    equal:(a,b)=>a.firstname===b.firstname
+  })
+
+  total = linkedSignal({
+    source: this.quantity,
+    computation: (previous, newdata) => {
+      console.log(previous);
+      console.log(newdata);
+      return this.quantity() * this.price()
+    }
+  })
+
+
+  total1 = computed(() => this.quantity() * this.price());
+
+  change(qty: any) {
+    this.quantity.set(qty);
+  }
+  namechange(fname: string, lastname: string) {
+    this.user.set({ firstname: fname, lastname: lastname })
+  }
 
 }
