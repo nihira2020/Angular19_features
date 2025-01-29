da2cc36e7f511e25f931874311fe2d4e1d54025b
Natarajan <nataraj.itsolution@gmail.com>
2025-01-29 13:23:04 +0530

    Files created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RAPIComponent } from './component/rapi/rapi.component';
+import { LinkComponent } from './component/link/link.component';
 
 export const routes: Routes = [
-    {path:'resouceapi',component:RAPIComponent}
+    { path: 'resouceapi', component: RAPIComponent },
+    { path: 'link', component: LinkComponent }
 ];
